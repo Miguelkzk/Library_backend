d2415dd5bd2070efe5a5d2922963c857e814fb71
Miguelkzk <141929749+Miguelkzk@users.noreply.github.com>
2024-01-20 20:04:59 -0300

    Update client.rb

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,5 @@
 class Client < ApplicationRecord
   has_many :book_rentals
-  validates  presence: true
   validates :name, :lastname, :card_id, :phone, presence: true
   validates :email, presence: true, format: { with: /\A\S+@\S+\z/ }
 end
